fd3f38fa1fa5d979ecfe6d4ca3bbe7dddf350d0f
Prasansha Ojha <ojhaprasansha63@gmail.com>
2025-02-09 19:09:56 +0545

    Last Commit

--- Hosting/Hosting/settings.py
+++ Hosting/Hosting/settings.py
@@ -130,11 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+import os
+
+STATIC_URL = "/static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [STATIC_DIR]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
